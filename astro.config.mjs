fe37fdc33cf8249201e6d8bd64d6136897d7ba59
anannayamustcode <anannayareallycodes@gmail.com>
2025-07-02 18:43:09 +0530

    ok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,24 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
 import node from '@astrojs/node';
 
 export default defineConfig({
   output: 'server',
-  adapter: node({ mode: 'standalone' }),
-  integrations: [tailwind(), react()],
+  adapter: node({
+    mode: 'standalone',
+  }),
+  server: {
+    port: process.env.PORT || 3000,
+  },
 });
 
-// // @ts-check
+
 // import { defineConfig } from 'astro/config';
 // import tailwind from '@astrojs/tailwind';
 // import react from '@astrojs/react';
-// import tailwind from '@astrojs/tailwind';
+// import node from '@astrojs/node';
 
-
-// // import tailwindcss from '@tailwindcss/vite';
-
-// // https://astro.build/config
 // export default defineConfig({
-//   integrations: [react(), tailwind()],
-
-//   vite: {
-//     plugins: [tailwindcss()]
-//   }
+//   output: 'server',
+//   adapter: node({ mode: 'standalone' }),
+//   integrations: [tailwind(), react()],
 // });
