2ef94947c4f12ee62c817e3e43759e4d5eccb55a
anannayamustcode <anannayareallycodes@gmail.com>
2025-07-02 19:41:10 +0530

    ok

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   output: 'server',
