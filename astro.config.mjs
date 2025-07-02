f4e441f1761149a593ed66d5ddf7dcdc62aed88e
anannayamustcode <anannayareallycodes@gmail.com>
2025-07-02 17:52:38 +0530

    deploy cahnges

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,8 @@ import node from '@astrojs/node';
 
 export default defineConfig({
   output: 'server',
-  adapter: node({ mode: 'standalone' }),
+  // adapter: node({ mode: 'standalone' }),
+  adapter: '@astrojs/node',
   integrations: [tailwind(), react()],
 });
 
