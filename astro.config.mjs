b2ca801248da3f526ea6f53fd5631d8758b7a063
anannayamustcode <anannayareallycodes@gmail.com>
2025-07-02 19:21:10 +0530

    Add React integration and specify Node.js engine version

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,43 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
+import react from '@astrojs/react';
 
 export default defineConfig({
   output: 'server',
   adapter: node({
-  mode: 'standalone',
+    mode: 'standalone',
   }),
+  integrations: [react()],
   server: {
-    host: '0.0.0.0', // 👈 THIS is crucial for Railway
+    host: '0.0.0.0',
     port: process.env.PORT || 3000,
   },
 });
 
 
+
 // import { defineConfig } from 'astro/config';
-// import tailwind from '@astrojs/tailwind';
-// import react from '@astrojs/react';
 // import node from '@astrojs/node';
 
 // export default defineConfig({
 //   output: 'server',
-//   adapter: node({ mode: 'standalone' }),
-//   integrations: [tailwind(), react()],
+//   adapter: node({
+//   mode: 'standalone',
+//   }),
+//   server: {
+//     host: '0.0.0.0',
+//     port: process.env.PORT || 3000,
+//   },
 // });
+
+
+// // import { defineConfig } from 'astro/config';
+// // import tailwind from '@astrojs/tailwind';
+// // import react from '@astrojs/react';
+// // import node from '@astrojs/node';
+
+// // export default defineConfig({
+// //   output: 'server',
+// //   adapter: node({ mode: 'standalone' }),
+// //   integrations: [tailwind(), react()],
+// // });
